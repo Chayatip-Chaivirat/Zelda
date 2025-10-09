f00eb9efa2e7b4582ee8780a15c84bb1dc0a6667
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2025-10-09 15:53:19 +0200

    Add classes and changed reference in Monogame

--- Zelda/TextureManager.cs
+++ Zelda/TextureManager.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Zelda
 {
     internal class TextureManager
     {
+        //===== Player =====
+        Texture2D playerTex;
+
+        //===== Enemy =====
+        Texture2D enemyTex;
+
+        public void Textures(ContentManager content)
+        {
+            playerTex = content.Load<Texture2D>("Link_all");
+            enemyTex = content.Load<Texture2D>("skelett");
+        }
     }
 }
