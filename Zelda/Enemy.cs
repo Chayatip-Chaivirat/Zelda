0b7e17268a0808104019c649efc09f30213fee2d
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2025-10-13 12:18:39 +0200

    Enemy.cs shell

--- Zelda/Enemy.cs
+++ Zelda/Enemy.cs
@@ -6,5 +6,36 @@ namespace Zelda
 {
     internal class Enemy
     {
+        public Texture2D enemyTex;
+        public Vector2 position;
+        public Rectangle enemyRec;
+        public int speed;
+
+        public Enemy(Texture2D enemyTex, Vector2 position, Rectangle enemyRec)
+        {
+            this.enemyTex = enemyTex;
+            this.position = position;
+            this.enemyRec = enemyRec;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void UpDownMovement(int speed)
+        {
+            position.Y += speed;
+        }
+
+        public void LeftRightMovement(int speed)
+        {
+            position.X += speed;
+        }
+        
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(enemyTex, position, enemyRec, Color.White);
+        }
     }
 }
